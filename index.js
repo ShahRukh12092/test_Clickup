20bd97ccc21ea19fde1e50bec8b905c622651006
ShahRukh12092 <87061064+ShahRukh12092@users.noreply.github.com>
2024-12-18 12:40:10 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const num1 =15
+const num2 =13
+const sum = num1 + num2
+const sub = num1 - num2
+const mul = num1 * num2
+const div = num1 / num2
+console.log(`Sum is ${sum}`)
+console.log(`Subtract is ${sub}`)
+console.log(`Multiply is ${mul}`)
+console.log(`Divide is ${div}`)
